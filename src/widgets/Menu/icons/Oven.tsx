dd7dccb90156211f53697c350fae536260abe44d
FlameFinance <84813207+FlameFinance@users.noreply.github.com>
2021-05-29 19:34:03 -0700

    Update Oven.tsx

--- src/widgets/Menu/icons/Oven.tsx
+++ src/widgets/Menu/icons/Oven.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-export const Oven = (props: React.SVGProps<SVGSVGElement>) => (
+const Oven = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 32 32"
@@ -12,3 +12,4 @@ export const Oven = (props: React.SVGProps<SVGSVGElement>) => (
     <path d="M80 310h220V130H80v180zm20-160h50v20h80v-20h50v140H100V150zM90 40h40v20H90zM170 40h40v20h-40zM250 40h40v20h-40z" />
   </svg>
 );
+export default Oven;

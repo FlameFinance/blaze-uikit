4ca1458cd70e29702aa70d11a13d92071e384274
FlameFinance <84813207+FlameFinance@users.noreply.github.com>
2021-05-29 19:34:21 -0700

    Update Bar.tsx

--- src/widgets/Menu/icons/Bar.tsx
+++ src/widgets/Menu/icons/Bar.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-export const Bar = (props: React.SVGProps<SVGSVGElement>) => (
+const Bar = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     width={32}
     height={32}
@@ -13,3 +13,4 @@ export const Bar = (props: React.SVGProps<SVGSVGElement>) => (
     <path d="M0 22h1v-5h4v5h2V12h4v10h2V7h4v15h2V1h4v21h1v1H0v-1zm4-4H2v4h2v-4zm6-5H8v9h2v-9zm6-5h-2v14h2V8zm6-6h-2v20h2V2z" />
   </svg>
 );
+export default Bar;

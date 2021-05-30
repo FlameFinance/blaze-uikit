789bc29a011fde3755dca855e31e7ec57b4778ae
FlameFinance <tarashanst@gmail.com>
2021-05-30 10:09:00 -0700

    update

--- dist/widgets/Menu/icons/index.d.ts
+++ dist/widgets/Menu/icons/index.d.ts
@@ -25,3 +25,6 @@ export { default as RoadmapIcon } from "./Roadmap";
 export { default as BondsIcon } from "./Bonds";
 export { default as VaultsIcon } from "./Vaults";
 export { default as EggHouseIcon } from "./EggHouse";
+export { default as BarIcon } from "./Bar";
+export { default as StoveIcon } from "./Stove";
+export { default as OvenIcon } from "./Oven";

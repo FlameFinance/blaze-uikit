801bd0d5e9105bd9861ed2f96227052c0ee6a486
FlameFinance <tarashanst@gmail.com>
2021-05-30 09:42:43 -0700

    hi

--- src/widgets/Menu/icons/Bar.tsx
+++ src/widgets/Menu/icons/Bar.tsx
@@ -3,16 +3,11 @@ import Svg from "../../../components/Svg/Svg";
 import { SvgProps } from "../../../components/Svg/types";
 
 const Icon: React.FC<SvgProps> = (props) => {
-  <svg
-    width={32}
-    height={32}
-    xmlns="http://www.w3.org/2000/svg"
-    fillRule="evenodd"
-    clipRule="evenodd"
-    viewBox="0 0 32 32"
-    {...props}
-  >
-    <path d="M0 22h1v-5h4v5h2V12h4v10h2V7h4v15h2V1h4v21h1v1H0v-1zm4-4H2v4h2v-4zm6-5H8v9h2v-9zm6-5h-2v14h2V8zm6-6h-2v20h2V2z" />
-  </svg>
-);
+  return (
+    <Svg viewBox="0 0 24 24" {...props}>
+      <path d="M0 22h1v-5h4v5h2V12h4v10h2V7h4v15h2V1h4v21h1v1H0v-1zm4-4H2v4h2v-4zm6-5H8v9h2v-9zm6-5h-2v14h2V8zm6-6h-2v20h2V2z" />
+    </Svg>
+  );
+};
+
 export default Icon;

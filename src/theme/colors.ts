6ccc1693495f2904b5471cf73de7fe074ad3dbc4
atarashansky <alexander.tarashansky@atarashansky-mba.local>
2021-06-19 18:18:37 -0700

    fix

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -48,7 +48,7 @@ export const darkColors: Colors = {
   textDisabled: "#666171",
   textSubtle: "#c9c4d4",
   borderColor: "#524B63",
-  card: "rgba(0.15,0.15,0.17,0.5)",
+  card: "rgba(0.72,0.72,0.72,0.5)",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
   },

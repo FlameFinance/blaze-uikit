1e5d347dcb15f1222d8507bb8af3ff56681b86c9
FlameFinance <alexander.tarashansky@atarashansky-mba.local>
2021-06-20 09:21:52 -0700

    fix

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -1889,8 +1889,8 @@ var Icon$s = function (props) {
 
 var Logo$1 = function (_a) {
     var isDark = _a.isDark, props = __rest(_a, ["isDark"]);
-    return (React__default['default'].createElement(Svg, __assign({ viewBox: "0 0 auto 50%" }, props),
-        React__default['default'].createElement("image", { width: "auto", height: "50%", href: isDark ? "/full-logo.png" : "/full-logo.png" })));
+    return (React__default['default'].createElement(Svg, __assign({ viewBox: "0 0 200 50" }, props),
+        React__default['default'].createElement("image", { width: "200", height: "50", href: isDark ? "/full-logo.png" : "/full-logo.png" })));
 };
 
 var Icon$r = function (props) {
--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -9,10 +9,10 @@ interface LogoProps extends SvgProps {
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-    <Svg viewBox="0 0 auto 50%" {...props}>
+    <Svg viewBox="0 0 200 50" {...props}>
       <image
-        width="auto"
-        height="50%"
+        width="200"
+        height="50"
         href={isDark ? "/full-logo.png" : "/full-logo.png"}
       />
     </Svg>

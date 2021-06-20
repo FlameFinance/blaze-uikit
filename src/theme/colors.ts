c0cfa6b4e6114d7463f7b84b3671fe58e052a252
FlameFinance <alexander.tarashansky@atarashansky-mba.local>
2021-06-20 10:09:43 -0700

    fix

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -17,7 +17,7 @@ export const brandColors = {
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#eb675b",
+  background: "#f1968e",
   backgroundDisabled: "#E9EAEB",
   contrast: "#191326",
   invertedContrast: "#FFFFFF",
